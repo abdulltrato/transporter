ec870b1f9e67ef57d92809fc10b8719072a913e5
Abdul Daniel Trato <abdultrato@gmail.com>
2026-04-26 18:24:18 +0200

    Update rides.module.ts

--- backend/src/rides/rides.module.ts
+++ backend/src/rides/rides.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DriversModule } from '../drivers/drivers.module';
 import { LocationModule } from '../location/location.module';
+import { RealtimeEventsModule } from '../realtime/events/realtime-events.module';
 import { RidesController } from './controllers/rides.controller';
 import { RidesRepository } from './repositories/rides.repository';
 import { DriverMatchingService } from './services/driver-matching.service';
 import { RidesService } from './services/rides.service';
 
 @Module({
-  imports: [DriversModule, LocationModule],
+  imports: [DriversModule, LocationModule, RealtimeEventsModule],
   controllers: [RidesController],
   providers: [RidesRepository, DriverMatchingService, RidesService],
   exports: [RidesService]
