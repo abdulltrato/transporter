a1a210d650f7021f703ac7c05c523d6ba5f2c726
Abdul Daniel Trato <abdultrato@gmail.com>
2026-04-26 18:24:05 +0200

    Update location.module.ts

--- backend/src/location/location.module.ts
+++ backend/src/location/location.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DriversModule } from '../drivers/drivers.module';
+import { RealtimeEventsModule } from '../realtime/events/realtime-events.module';
 import { LocationController } from './controllers/location.controller';
 import { LocationService } from './services/location.service';
 import { LocationStore } from './store/location.store';
 
 @Module({
-  imports: [DriversModule],
+  imports: [DriversModule, RealtimeEventsModule],
   controllers: [LocationController],
   providers: [LocationStore, LocationService],
   exports: [LocationService]
